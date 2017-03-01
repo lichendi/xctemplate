fdace7a5358c553b3b91ec8be2775be4d282ce9e
LiChendi <lichendi@NumLock.local>
2017-03-01 12:24:48 +0800

    add UIViewController

---
+++ Cocoa Touch Class.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,40 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - property
+
+#pragma mark - life cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+#pragma mark - UITableViewDelegate
+
+#pragma mark - CustomDelegate
+
+#pragma mark - event response
+
+#pragma mark - private methods
+
+#pragma mark - getters and setters
+
+@end
---
+++ Cocoa Touch Class.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
@@ -0,0 +1,35 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+// MARK: - property -
+    
+    
+// MARK: - life cycle -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
+
+
+// MARK: - UITableViewDelegate -
+
+// MARK: - CustomDelegate -
+
+// MARK: - event response -
+
+// MARK: - private methods -
